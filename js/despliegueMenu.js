2bd0e63982b42157599079da7e15d9797c476263
miiguelngl <04mangel@gmail.com>
2024-04-12 09:58:59 +0200

    Cierre del menú

--- js/despliegueMenu.js
+++ js/despliegueMenu.js
@@ -1,10 +1,26 @@
 window.onload(new function() {
-    document.getElementById("header-boton-abrir").addEventListener("click", function() {
-        document.getElementById("header-menu").classList.add("active");
-    });
+    document.addEventListener('DOMContentLoaded', function() {
+        const botonAbrir = document.getElementById('header-boton-abrir');
+        const botonCerrar = document.getElementById('header-boton-cerrar');
+        const menuDesplegable = document.getElementById('header-menu');
+        
+        botonAbrir.addEventListener('click', function() {
+            menuDesplegable.classList.add('active');
+        });
+        
+        botonCerrar.addEventListener('click', function() {
+            menuDesplegable.classList.remove('active');
+        });
     
-    document.getElementById("header-boton-cerrar").addEventListener("click", function() {
-        document.getElementById("header-menu").classList.remove("active");
+        window.addEventListener('click', function(event) {
+            const targetElement = event.target;
+            const screenWidth = window.innerWidth;
+            const clickX = event.clientX;
+    
+            if (targetElement !== botonAbrir && targetElement !== botonCerrar &&
+                clickX < screenWidth * 0.4) {
+                menuDesplegable.classList.remove('active');
+            }
+        });
     });
 })
-
