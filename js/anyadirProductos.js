36d7da9f3889066c25272678ee707a516a51f1c6
miiguelngl <04mangel@gmail.com>
2024-04-29 14:51:25 +0200

    Ventana emergente - Cambio nombre archivos burgers

--- js/anyadirProductos.js
+++ js/anyadirProductos.js
@@ -93,3 +93,67 @@ function vaciarCarrito() {
 
 // Captura el clic en el botón "Vaciar Carrito" y llama a la función para vaciarlo
 document.getElementById('vaciarCarrito').addEventListener('click', vaciarCarrito);
+
+
+
+//VENTANA EMERGENTE
+document.addEventListener('DOMContentLoaded', function() {
+    const lupa = document.querySelectorAll('#lupa');
+    const botonCerrar = document.querySelector('.cerrar');
+
+    lupa.forEach(function(lupaItem) {
+        lupaItem.addEventListener('click', function() {
+            const nombre = lupaItem.dataset.nombre;
+            const descripcion = lupaItem.dataset.descripcion;
+            const imagenSrc = 'img/Hamburguesas/' + nombre + '.png';
+
+            const ventanaEmergente = document.createElement('div');
+            ventanaEmergente.classList.add('ventana-emergente');
+
+            const contenidoVentana = document.createElement('div');
+            contenidoVentana.classList.add('contenido-ventana');
+
+            const botonCerrarVentana = document.createElement('span');
+            botonCerrarVentana.classList.add('cerrar');
+            botonCerrarVentana.innerHTML = `
+                <svg fill="#000000" height="35px" width="35px" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 460.775 460.775" xml:space="preserve">
+                    <path fill="#e5d4d4" d="M285.08,230.397L456.218,59.27c6.076-6.077,6.076-15.911,0-21.986L423.511,4.565c-2.913-2.911-6.866-4.55-10.992-4.55
+                        c-4.127,0-8.08,1.639-10.993,4.55l-171.138,171.14L59.25,4.565c-2.913-2.911-6.866-4.55-10.993-4.55
+                        c-4.126,0-8.08,1.639-10.992,4.55L4.558,37.284c-6.077,6.075-6.077,15.909,0,21.986l171.138,171.128L4.575,401.505
+                        c-6.074,6.077-6.074,15.911,0,21.986l32.709,32.719c2.911,2.911,6.865,4.55,10.992,4.55c4.127,0,8.08-1.639,10.994-4.55
+                        l171.117-171.12l171.118,171.12c2.913,2.911,6.866,4.55,10.993,4.55c4.128,0,8.081-1.639,10.992-4.55l32.709-32.719
+                        c6.074-6.075,6.074-15.909,0-21.986L285.08,230.397z"/>
+                </svg>
+            `;
+
+            const imagenHamburguesa = document.createElement('img');
+            imagenHamburguesa.src = imagenSrc;
+            imagenHamburguesa.alt = 'Imagen de la hamburguesa';
+
+            const nombreHamburguesa = document.createElement('h2');
+            nombreHamburguesa.textContent = nombre;
+
+            const descripcionHamburguesa = document.createElement('p');
+            descripcionHamburguesa.textContent = descripcion;
+
+            contenidoVentana.appendChild(botonCerrarVentana);
+            contenidoVentana.appendChild(imagenHamburguesa);
+            contenidoVentana.appendChild(nombreHamburguesa);
+            contenidoVentana.appendChild(descripcionHamburguesa);
+            ventanaEmergente.appendChild(contenidoVentana);
+            document.body.appendChild(ventanaEmergente);
+
+            ventanaEmergente.style.display = 'block';
+
+            botonCerrarVentana.addEventListener('click', function() {
+                ventanaEmergente.style.display = 'none';
+                ventanaEmergente.innerHTML = '';
+            });
+        });
+    });
+
+    botonCerrar.addEventListener('click', function() {
+        const ventanaEmergente = document.querySelector('.ventana-emergente');
+        ventanaEmergente.style.display = 'none';
+    });
+});
