02ae81ec7ad17accb78d410975385a82c24b8d6b
miiguelngl <04mangel@gmail.com>
2024-05-01 19:06:59 +0200

    Ingredientes + Muestra el precio en el carrito

--- js/productos.js
+++ js/productos.js
@@ -11,11 +11,12 @@ function guardarCarritoLocalStorage(carrito) {
 }
 
 // Función para añadir hamburguesa al carrito
-function añadirAlCarrito(nombreBurger) {
+function añadirAlCarrito(nombreBurger, precioBurger) {
     let carrito = obtenerCarritoLocalStorage();
     let burger = {
         id: idBurger,
-        nombre: nombreBurger
+        nombre: nombreBurger,
+        precio: precioBurger
     };
     idBurger++;
     carrito.push(burger); // Añadir nombre de la hamburguesa al carrito
@@ -50,6 +51,7 @@ function actualizarCarrito() {
     let carrito = obtenerCarritoLocalStorage();
 
     carrito.forEach(producto => {
+        console.log(producto.precio);
         let item = document.createElement('li');
         let x = document.createElement("span");
         x.innerHTML = '<svg fill="#000000" height="16px" width="16px" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 460.775 460.775" xml:space="preserve"><path fill="#e5d4d4" d="M285.08,230.397L456.218,59.27c6.076-6.077,6.076-15.911,0-21.986L423.511,4.565c-2.913-2.911-6.866-4.55-10.992-4.55c-4.127,0-8.08,1.639-10.993,4.55l-171.138,171.14L59.25,4.565c-2.913-2.911-6.866-4.55-10.993-4.55c-4.126,0-8.08,1.639-10.992,4.55L4.558,37.284c-6.077,6.075-6.077,15.909,0,21.986l171.138,171.128L4.575,401.505c-6.074,6.077-6.074,15.911,0,21.986l32.709,32.719c2.911,2.911,6.865,4.55,10.992,4.55c4.127,0,8.08-1.639,10.994-4.55l171.117-171.12l171.118,171.12c2.913,2.911,6.866,4.55,10.993,4.55c4.128,0,8.081-1.639,10.992-4.55l32.709-32.719c6.074-6.075,6.074-15.909,0-21.986L285.08,230.397z"/></svg>';
@@ -58,6 +60,7 @@ function actualizarCarrito() {
         item.textContent = producto.nombre;
         item.appendChild(x);
         listaProductos.appendChild(item);
+        total += parseFloat(producto.precio);
     });
 
     var xs = document.querySelectorAll("#eliminar");
@@ -70,14 +73,14 @@ function actualizarCarrito() {
         });
     });
 
-    document.getElementById('total').textContent = `Total: ${total.toFixed(2)}€`;
+    document.getElementById('total').textContent = `Total: ${parseFloat(total).toFixed(2)}€`;
 }
 
 
 var buttons = document.querySelectorAll(".añadir");
 buttons.forEach(button => {
     // Agrega eventos de clic para los demás botones "Añadir" aquí...
-    button.addEventListener('click', () => añadirAlCarrito(button.getAttribute('name')));
+    button.addEventListener('click', () => añadirAlCarrito(button.getAttribute('name'), button.dataset.precio));
 });
 
 
@@ -149,8 +152,9 @@ document.addEventListener('DOMContentLoaded', function() {
             botonHamburguesa.textContent = 'La quiero';
             botonHamburguesa.classList.add('añadir');
             botonHamburguesa.id = 'comprar';
+            botonHamburguesa.dataset.precio = precio;
             botonHamburguesa.setAttribute('name', nombre);
-            botonHamburguesa.addEventListener('click', () => añadirAlCarrito(botonHamburguesa.getAttribute('name')));
+            botonHamburguesa.addEventListener('click', () => añadirAlCarrito(botonHamburguesa.getAttribute('name'), botonHamburguesa.dataset.precio));
 
             contenidoPrincipalVentana.appendChild(botonCerrarVentana);
             contenidoPrincipalVentana.appendChild(imagenHamburguesa);
@@ -163,6 +167,20 @@ document.addEventListener('DOMContentLoaded', function() {
 
             const separador = document.createElement('hr');
             contenidoVentana.appendChild(separador);
+
+            const listaIngredientesTittle = document.createElement('h2');
+            listaIngredientesTittle.textContent = 'Ingredientes';
+            contenidoVentana.appendChild(listaIngredientesTittle);
+            
+            const ingredientes = lupaItem.previousSibling.previousSibling.dataset.ingredientes;
+            const ingredientesArray = ingredientes.split(', ');
+            const listaUL = document.createElement('ul');
+            ingredientesArray.forEach(ingrediente => {
+                let listaLI = document.createElement('li');
+                listaLI.textContent = ingrediente;
+                listaUL.appendChild(listaLI);
+            });
+            contenidoVentana.appendChild(listaUL);
 
             ventanaEmergente.appendChild(contenidoVentana);
             document.body.appendChild(ventanaEmergente);
