cc859f8d6b35e946c064127b34bf88e83754687b
miiguelngl <04mangel@gmail.com>
2024-05-03 14:42:41 +0200

    Añadir icono user

--- php/header.php
+++ php/header.php
@@ -27,6 +27,13 @@
                  if(isset($_SESSION['Usu'])){
                     echo '
                     <div id="buttons-header-case">
+                        <button id="registroButton">
+                            <a href="" id="">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" fill="#FEBD01" width="40px" height="40px">
+                                    <path d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z"/>
+                                </svg>
+                            </a>
+                        </button>
                         <img src="img/cart_burger.svg" alt="" id="icon-cart">
                         <button class="header-boton" id="header-boton-abrir">
                             <svg width="40px" height="40px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
