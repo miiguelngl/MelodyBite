ca0656d36b51c8193d0ee2d06a12e5fe90b9e474
miiguelngl <04mangel@gmail.com>
2024-06-17 22:49:10 +0200

    Formulario de contacto reserva hecho

--- contacto.php
+++ contacto.php
@@ -20,19 +20,52 @@
                 <h2>Reserva en uno de nuestros locales</h2>
             </div>
             <div id="form-contact">
-                <form action="">
-                    <label for="">Nombre</label>
-                    <input type="text" placeholder="Nombre" name="nombre">
-                    <label for="">Apellidos</label>
-                    <input type="text" placeholder="Apellidos" name="apellidos">
+                <form action="php/hacerReserva.php" method="post">
+                    <div class="centrar">
+                        <div class="centrarInput">
+                            <label for="">Nombre</label>
+                            <input type="text" placeholder="Nombre" name="nombre">
+                         </div>
+                        <div class="centrarInput">
+                            <label for="">Apellidos</label>
+                            <input type="text" placeholder="Apellidos" name="apellidos">
+                        </div>
+                    </div>
+                    <label for="">Correo</label>
+                    <input type="email" placeholder="Correo" name="correo">
                     <label for="">Local</label>
                     <select name="local">
                         <option value="Gran via" selected>Gran vía</option>
                         <option value="Campanar">Campanar</option>
                         <option value="Aragon">Aragón</option>
                     </select>
-                    <label for="">Fecha</label>
-                    <input type="date" min="<?php echo date('Y-m-d'); ?>" name="fecha">
+                    <div class="centrar">
+                        <div class="centrarInput">
+                            <label for="">Fecha</label>
+                            <input type="date" min="<?php echo date('Y-m-d'); ?>" name="fecha">
+                         </div>
+                        <div class="centrarInput">
+                            <label for="">Hora</label>
+                            <select name="hora">
+                                <option value="13:00" selected>13:00</option>
+                                <option value="13:30" selected>13:30</option>
+                                <option value="14:00" selected>14:00</option>
+                                <option value="14:30" selected>14:30</option>
+                                <option value="15:00" selected>15:00</option>
+                                <option value="15:30" selected>15:30</option>
+                                <option value="16:00" selected>16:00</option>
+                                <option value="16:30" selected>16:30</option>
+                                <option value="19:00" selected>19:00</option>
+                                <option value="19:30" selected>19:30</option>
+                                <option value="20:00" selected>20:00</option>
+                                <option value="20:30" selected>20:30</option>
+                                <option value="21:00" selected>21:00</option>
+                                <option value="21:30" selected>21:30</option>
+                                <option value="22:00" selected>22:00</option>
+                                <option value="22:30" selected>22:30</option>
+                            </select>
+                        </div>
+                    </div>
                     <label for="">Personas</label>
                     <input type="number" max="8" name="numPersonas">
                     <input type="submit" class="finalizarCompra" value="HACER RESERVA">
--- php/hacerReserva.php
+++ php/hacerReserva.php
@@ -3,10 +3,13 @@
     ini_set('display_errors', '1');
 
     /* ENVIO */
+    
     $nombreCliente = $_POST["nombre"];
     $apellidoCliente = $_POST["apellidos"];
+    $correo = $_POST["correo"];
     $local = $_POST["local"];
     $fecha = $_POST["fecha"];
+    $hora = $_POST["hora"];
     $numPersonas = $_POST["numPersonas"];
 
     $nombreCompleto = $nombreCliente . " " . $apellidoCliente;
@@ -17,7 +20,8 @@
     session_start();
     //Comprueba si existe sesion iniciada
     if (isset($_SESSION['Usu'])) {
-        enviarMailCliente($correo, $nombreCompleto, $local, $fecha, $numPersonas);
+        enviarMailCliente($correo, $nombreCliente, $nombreCompleto, $local, $fecha, $hora, $numPersonas);
+        header("Location: ../index.php");
     } else {
         // No hay sesión iniciada, redirigir a la página de inicio de sesión
         header("Location: ../formulario/IniciarSesion/signIn.html");
@@ -28,7 +32,7 @@
     use PHPMailer\PHPMailer\PHPMailer;
     use PHPMailer\PHPMailer\Exception;
 
-    function enviarMailCliente($correo, $nombreCompleto, $local, $fecha, $numPersonas){
+    function enviarMailCliente($correo, $nombreCliente, $nombreCompleto, $local, $fecha, $hora, $numPersonas){
         //Carga de las clases necesarias
         require '../mail/PHPMailer/src/Exception.php';
         require '../mail/PHPMailer/src/PHPMailer.php';
@@ -49,19 +53,45 @@
             //Remitente
             $mail->setFrom('rhapsodysburgers@outlook.es', 'Rhapsody´s Burgers');
             //Receptores. Podemos añadir más de uno. El segundo argumento es opcional, es el nombre
-            $mail->addAddress($correo, $nombre);     //Add a recipient
+            $mail->addAddress($correo, $nombreCliente);     //Add a recipient
             //Contenido
             //Si enviamos HTML
             $mail->isHTML(true);    
             $mail->CharSet = "UTF8";    
             //Asunto
-            $mail->Subject = '¡Tu bocado está más cerca!';
+            $mail->Subject = '¡Estamos mirando tu reserva en '. $local .'!';
             //Conteido HTML
-            $mail->Body    = '<h3>¡Hola '. $nombre .', tu pedido está ya en cocina!</h3><br><p>Puedes seguir el proceso del pedido en tu perfil.</p>';
+            $mail->Body    = '<h3>¡Hola '. $nombreCliente .'!<br>Estamos viendo la disponibilidad para la reserva de '. $local . ' a las ' . $hora .' el día ' . $fecha . ' para ' . $numPersonas .'.</h3><p>Nos pondremos en contacto contigo para confirmarte la reserva.</p><p>¡Un saludo!</p>';
             //Contenido alternativo en texto simple
-            $mail->AltBody = '¡Hola '. $nombre .', tu cuenta a sido creado correctamente. Ahora podras disfrutar de todos los servicios que ofrecemos';
+            $mail->AltBody = '¡Hola '. $nombreCliente .', tu cuenta a sido creado correctamente. Ahora podras disfrutar de todos los servicios que ofrecemos';
             //Enviar correo
             $mail->send();
+            echo 'El mensaje se ha enviado con exito';
+
+            $mail2 = new PHPMailer(true);
+            $mail2->isSMTP();                                           //Para usaar SMTP
+            $mail2->Host       = 'smtp-mail.outlook.com';                     //Nuestro servidor SMTMP smtp.gmail.com en caso de usar gmail
+            $mail2->SMTPAuth   = true;    
+            $mail2->Username   = 'rhapsodysburgers@outlook.es';             
+            $mail2->Password   = 'hrvghyjybswnfche';    
+            $mail2->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+            $mail2->Port = 587;
+            //Remitente
+            $mail2->setFrom('rhapsodysburgers@outlook.es', 'Rhapsody´s Burgers');
+            //Receptores. Podemos añadir más de uno. El segundo argumento es opcional, es el nombre
+            $mail2->addAddress('rhapsodysburgers@outlook.es', 'Reserva');     //Add a recipient
+            //Contenido
+            //Si enviamos HTML
+            $mail2->isHTML(true);    
+            $mail2->CharSet = "UTF8";    
+            //Asunto
+            $mail2->Subject = '¡Nueva reserva de '. $nombreCompleto .'!';
+            //Conteido HTML
+            $mail2->Body    = '<h3>Correo:</h3><h4>'. $correo .'</h4><h3>Nombre:</h3><h4>'. $nombreCompleto .'</h4><h3>Local:</h3><h4>'. $local .'</h4><h3>Fecha:</h3><h4>'. $fecha .'</h4><h3>Hora:</h3><h4>'. $hora .'</h4><h3>Personas:</h3><h4>'. $numPersonas .'</h4>';
+            //Contenido alternativo en texto simple
+            $mail2->AltBody = '¡Hola '. $nombreCliente .', tu cuenta a sido creado correctamente. Ahora podras disfrutar de todos los servicios que ofrecemos';
+            //Enviar correo
+            $mail2->send();
             echo 'El mensaje se ha enviado con exito';
         } catch (Exception $e) {
             echo "El mensaje no se ha enviado: {$mail->ErrorInfo}";
